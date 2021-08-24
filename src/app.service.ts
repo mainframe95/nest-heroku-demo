05cb9e5434fdacdc27a287294547ccaed0467381
Junior  MENSAH <jmensah@dactech-smb.lan>
2021-08-24 14:20:28 +0000

    first commit

---
+++ src/app.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { PRODUCTS } from './mock/products.mock';
+
+@Injectable()
+export class AppService {
+
+  products = PRODUCTS;
+
+  async getProducts() {
+    return await this.products;
+  }
+
+  getHello(): string {
+    return 'Hello World!';
+  }
+}
